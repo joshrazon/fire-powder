807110522c55253449779013f5fc74a744ca4a8a
Josh <joshrazonn@gmail.com>
2020-11-05 13:56:40 -0700

    created ejs

--- server.js
+++ server.js
@@ -4,30 +4,11 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// The standard HTTP methods
-app.get('/',function(request,response){
-  console.log(request.method);
+app.set('view engine', 'ejs');
 
-  // response is magically turned into JSON. express middlewares are the bees knees
-  response.send({name: 'Josh', age: 24});
-});
-
-app.post('/',function(request,response){
-  response.send('POST request: Create data');
-});
-
-app.put('/',function(request,response){
-  response.send('PUT request: Update data');
-});
-
-app.delete('/',function(request,response){
-  response.send('DELETE request: Delete data');
-});
-
-app.use(function(req, res) {
-  res.status(404);
-  res.send('404: File Not Found');
-});
+app.get('/', function(request, response) {
+  res.render('index', {data: {userQuery: request.params.title}});
+})
 
 const PORT = process.env.PORT || 8080;
 
